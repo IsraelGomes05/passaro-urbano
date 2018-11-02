d33c1beaf58093ce9395fc558b143f5f8164e303
IsraelGomes05 <israelgomes05@gmail.com>
2018-11-02 16:39:10 -0400

    Utilizando URL global

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -1,28 +1,30 @@
 import {Oferta} from './shared/oferta.module';
 import {Http} from '@angular/http';
 import {Injectable} from '@angular/core';
+import {URL_API} from './app.api';
 
 @Injectable()
 export class OfertasService {
+
 
   constructor(private http: Http) {
   }
 
   public getOfertas(): Promise<Oferta[]> {
     // efetuar requisição
-    return this.http.get(' http://localhost:3000/ofertas?destaque=true')
+    return this.http.get(`${URL_API}?destaque=true`)
       .toPromise()
       .then((resposta: any) => resposta.json());
   }
 
   public getOfertasPorCategorias(categoria: string): Promise<Oferta[]> {
-    return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+    return this.http.get(`${URL_API}?categoria=${categoria}`)
       .toPromise()
       .then((resposta: any) => resposta.json());
   }
 
   getOfertaPorId(param: any): Promise<Oferta> {
-    return this.http.get(`http://localhost:3000/ofertas?id=${param}`)
+    return this.http.get(`${URL_API}?id=${param}`)
       .toPromise()
       .then((resposta: any) => resposta.json()[0]);
   }
